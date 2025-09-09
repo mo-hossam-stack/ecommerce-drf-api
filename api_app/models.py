ae9a2d22e81237771306b1e6b6875d48b7811bdb
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-09-09 23:45:35 +0300

    feat(models): add slug field to Product and Category

--- api_app/models.py
+++ api_app/models.py
@@ -9,7 +9,7 @@ class CustomUser(AbstractUser):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True, blank=True)
     image = models.ImageField(upload_to="category_img", blank=True)
     def __str__(self):
         return self.name
@@ -18,7 +18,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True, blank=True)
     image = models.ImageField(upload_to="product_img", blank=True)
     featured = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name="products", blank=True, null=True)

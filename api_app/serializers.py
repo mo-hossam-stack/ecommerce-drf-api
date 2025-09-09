d0eb402e6009495b4a4efdaae1cb1f12471808b5
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-09-09 23:49:32 +0300

    feat(serializers): add ProductDetailSerializer and CategoryListSerializer  and correct model in category list/detail

--- api_app/serializers.py
+++ api_app/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
 from .models import Product,Category
-class ProductSerializer(serializers.ModelSerializer):
+class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ["id", "name", "slug", "image","description", "price"]
+        fields = ["id", "name", "slug", "image", "price"]
 
-class CategorySerializer(serializers.ModelSerializer):
-    product = ProductSerializer(many=True, read_only=True)
+class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = ["id", "name", "description","slug", "image", "price"]
+        
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    product = ProductListSerializer(many=True, read_only=True)
+    class Meta:
+        model = Category
         fields = ["id", "name", "image", "products"]
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name", "image", "slug"]
+

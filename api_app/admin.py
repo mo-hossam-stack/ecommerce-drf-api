f4e8657da465d91d2ddcf2c157e788a76af4d8f6
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-09-10 23:13:32 +0300

    Register the models in admin panel

--- api_app/admin.py
+++ api_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import  Category, CustomUser, Product
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "first_name", "last_name")
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "featured"]
+
+admin.site.register(Product, ProductAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+
+admin.site.register(Category, CategoryAdmin)

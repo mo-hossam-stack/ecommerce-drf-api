410da3c4e2d941d7f2ff0d9f31ee8db62bb8f04f
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-09-10 23:14:19 +0300

    include api_app urls

--- api_project/urls.py
+++ api_project/urls.py
@@ -5,6 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('',include('api_app.urls'))
+    path('',include('api_app.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

bf5f05acedf08a8d72e5ecd249c5e13b9d365a22
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-09-24 21:28:58 +0300

    implement Cart and CartItem models

--- api_app/models.py
+++ api_app/models.py
@@ -51,3 +51,20 @@ class Product(models.Model):
             self.slug = unique_slug
         
         super().save(*args, **kwargs)
+
+class Cart(models.Model):
+    cart_code = models.CharField(max_length=11, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.cart_code
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="cartitems")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="item")
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in cart {self.cart.cart_code}"

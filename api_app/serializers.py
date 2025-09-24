8ea4553280bf593d0362b3d91735d64fa1496d6e
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-09-24 21:30:22 +0300

    add cartitem,cart stat and Cart serializers

--- api_app/serializers.py
+++ api_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product,Category
+from .models import Cart, CartItem, Product,Category
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
@@ -21,3 +21,39 @@ class CategoryListSerializer(serializers.ModelSerializer):
         model = Category
         fields = ["id", "name", "image", "slug"]
 
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductListSerializer(read_only=True)
+    sub_total = serializers.SerializerMethodField()
+    class Meta:
+        model = CartItem 
+        fields = ["id", "product", "quantity", "sub_total"]
+
+    
+    def get_sub_total(self, cartitem):
+        total = cartitem.product.price * cartitem.quantity 
+        return total
+
+
+
+class CartSerializer(serializers.ModelSerializer):
+    cartitems = CartItemSerializer(read_only=True, many=True)
+    cart_total = serializers.SerializerMethodField()
+    class Meta:
+        model = Cart 
+        fields = ["id", "cart_code", "cartitems", "cart_total"]
+
+    def get_cart_total(self, cart):
+        items = cart.cartitems.all()
+        total = sum([item.quantity * item.product.price for item in items])
+        return total
+
+class CartStatSerializer(serializers.ModelSerializer): 
+    total_quantity = serializers.SerializerMethodField()
+    class Meta:
+        model = Cart 
+        fields = ["id", "cart_code", "total_quantity"]
+
+    def get_total_quantity(self, cart):
+        items = cart.cartitems.all()
+        total = sum([item.quantity for item in items])
+        return total
